9be6385d97a540e394032893baca29fd27651618
coiso <buesimples@gmail.com>
2021-02-15 23:03:02 +0000

    little cleanup

--- graphql-server/state/resolvers/query.js
+++ graphql-server/state/resolvers/query.js
@@ -1,10 +1,5 @@
-const { gql } = require("apollo-server");
-// const GraphQLJSON = require("graphql-type-json");
-
 module.exports = {
-  // JSON: GraphQLJSON,
   Query: {
-    //EOD
     searchStocks: async (_source, params, { dataSources }) =>
       await dataSources.Ours.searchStocks(params),
     getStockByCode: async (_source, params, { dataSources }) =>
@@ -13,17 +8,18 @@ module.exports = {
       dataSources.Ours.getAggregate(params),
     getAggregateForStock: async (_source, params, { dataSources }) =>
       dataSources.Ours.getAggregateForStock(params),
+    removeEmptyAndDuplicates: async (_source, params, { dataSources }) =>
+      dataSources.Ours.removeEmptyAndDuplicates(params),
+    getDistinctStockNames: async (_source, params, { dataSources }) =>
+      dataSources.Ours.getDistinctStockNames(params),
+
+    getCurrencyToCurrencyTimeseries: async (_source, params, { dataSources }) =>
+      dataSources.EODDataAPI.getCurrencyToCurrencyTimeseries(params),
     getExchangeStocks: async (_source, params, { dataSources }) =>
       dataSources.EODDataAPI.getExchangeStocks(params),
     getAllExchanges: async (_source, params, { dataSources }) =>
       dataSources.EODDataAPI.getAllExchanges(params),
     updateStocksCompletely: async (_source, params, { dataSources }) =>
       dataSources.EODDataAPI.updateStocksCompletely(params),
-    removeEmptyAndDuplicates: async (_source, params, { dataSources }) =>
-      dataSources.EODDataAPI.removeEmptyAndDuplicates(params),
-    getDistinctStockNames: async (_source, params, { dataSources }) =>
-      dataSources.Ours.getDistinctStockNames(params),
-    getCurrencyToCurrencyTimeseries: async (_source, params, { dataSources }) =>
-      dataSources.EODDataAPI.getCurrencyToCurrencyTimeseries(params),
   },
 };
